a2193e536af9837a5bdab80c1dc241c3ae0582af
BrianJayD <brian.jay1795@gmail.com>
2021-02-08 03:19:26 -0500

    Initial code for Firebare-Auth - Added firebase-auth to gradle. - Override onStart in LoginActivity for firebase-auth auto log in. (Commented until tested) - Added signIn function in LoginFragment.

--- app/src/main/java/com/gb/carspot/fragments/LoginFragment.java
+++ app/src/main/java/com/gb/carspot/fragments/LoginFragment.java
@@ -3,16 +3,24 @@ package com.gb.carspot.fragments;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.gb.carspot.R;
 import com.gb.carspot.activities.LoginActivity;
 import com.gb.carspot.activities.MainActivity;
 import com.gb.carspot.utils.Utils;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.transition.TransitionInflater;
 
@@ -28,6 +36,8 @@ public class LoginFragment extends Fragment
 {
     private final String TAG = getClass().getCanonicalName();
     private View rootView;
+
+    private FirebaseAuth mAuth;
 
     private Button loginButton;
 
@@ -54,6 +64,7 @@ public class LoginFragment extends Fragment
             setSharedElementEnterTransition(TransitionInflater.from(getContext()).inflateTransition(R.transition.move));
             setSharedElementReturnTransition(TransitionInflater.from(getContext()).inflateTransition(R.transition.move));
         }
+
     }
 
     @Override
@@ -68,6 +79,8 @@ public class LoginFragment extends Fragment
     private void setup()
     {
         loginButton = rootView.findViewById(R.id.login_button);
+
+        mAuth = FirebaseAuth.getInstance();
         Utils.setHaptic(loginButton);
         loginButton.setOnClickListener(new View.OnClickListener()
         {
@@ -84,5 +97,27 @@ public class LoginFragment extends Fragment
         Intent intent = new Intent(getContext(), LoginActivity.class);
         intent.setAction(ACTION_LOAD_MAIN_PAGE);
         startActivity(intent);
+    }
+
+    private void signIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "Sign in successful");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(getContext(), "Authentication success.",
+                                    Toast.LENGTH_SHORT).show();
+                            gotoMain();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "Log in failed", task.getException());
+                            Toast.makeText(getContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
