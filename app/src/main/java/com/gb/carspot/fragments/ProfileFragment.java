5c0a0962a8c5f79f5c03b8df2910805f5802f478
BrianJayD <brian.jay1795@gmail.com>
2021-02-15 21:22:43 -0500

    Profile now updates when changing values. TODO: Email update does not work, yet.

--- app/src/main/java/com/gb/carspot/fragments/ProfileFragment.java
+++ app/src/main/java/com/gb/carspot/fragments/ProfileFragment.java
@@ -5,19 +5,25 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 
 import com.gb.carspot.R;
 import com.gb.carspot.activities.LoginActivity;
 import com.gb.carspot.activities.MainActivity;
 import com.gb.carspot.models.User;
 import com.gb.carspot.repositories.UserRepository;
+import com.gb.carspot.utils.Utils;
 import com.gb.carspot.viewmodels.LoginActivityViewModel;
 import com.gb.carspot.viewmodels.MainActivityViewModel;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
@@ -39,6 +45,11 @@ import java.util.List;
 import static com.gb.carspot.utils.Constants.ACTION_LOAD_LOGIN_PAGE;
 import static com.gb.carspot.utils.Constants.ACTION_LOAD_MANAGE_PLATES_PAGE;
 import static com.gb.carspot.utils.Constants.ACTION_LOAD_PROFILE_PAGE;
+import static com.gb.carspot.utils.Constants.FIELD_EMAIL;
+import static com.gb.carspot.utils.Constants.FIELD_FIRST_NAME;
+import static com.gb.carspot.utils.Constants.FIELD_LAST_NAME;
+import static com.gb.carspot.utils.Constants.FIELD_PASSWORD;
+import static com.gb.carspot.utils.Constants.FIELD_PHONE;
 import static com.gb.carspot.utils.Constants.LOGIN_CURRENT_USER;
 import static com.gb.carspot.utils.Constants.SHARED_PREF_NAME;
 
@@ -69,6 +80,17 @@ public class ProfileFragment extends Fragment
     private TextInputLayout inputLayoutEnterPlate;
     private MaterialButton btnManagePlates;
     private MaterialButton btnSignUp;
+
+    private TextInputLayout firstNameInputLayout;
+    private TextInputLayout lastNameInputLayout;
+    private TextInputLayout emailInputLayout;
+    private TextInputLayout phoneInputLayout;
+    private TextInputLayout passwordInputLayout;
+    private TextInputLayout confirmInputLayout;
+
+    private User currentUserInfo;
+    private boolean okayToError = false;
+    private FirebaseUser currentFirebaseUser;
 
     public ProfileFragment()
     {
@@ -105,8 +127,8 @@ public class ProfileFragment extends Fragment
         super.onStart();
 
         //Check to see if there is a current user
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-
+        currentFirebaseUser = mAuth.getCurrentUser();
+        setupUserInfo();
     }
 
     @Override
@@ -140,17 +162,18 @@ public class ProfileFragment extends Fragment
             //if user is already in go to main activity
             btnManagePlates.setVisibility(View.VISIBLE);
             inputLayoutEnterPlate.setVisibility(View.INVISIBLE);
-            setupUserInfo(savedInstanceState);
+            setupUserInfo();
             btnSignUp.setText("Update Account Info");
             btnSignUp.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     Log.d(TAG, "Updating account");
-
+                    updateAccount();
                 }
             });
         } else {
             Log.d(TAG, "Load fragment_add_license_plates");
+            setupTextFieldsForEditing();
             btnManagePlates.setVisibility(View.INVISIBLE);
             inputLayoutEnterPlate.setVisibility(View.VISIBLE);
             btnSignUp.setOnClickListener(new View.OnClickListener() {
@@ -173,9 +196,10 @@ public class ProfileFragment extends Fragment
         startActivity(intent);
     }
 
-    private void setupUserInfo(Bundle savedInstanceState) {
+    private void setupUserInfo() {
         if(getContext() != null) {
             this.mainActivityViewModel = ((MainActivity) getActivity()).getViewModel();
+            setupTextFieldsForEditing();
         }
 
         mainActivityViewModel.getUser().observe(getActivity(), new Observer<User>() {
@@ -185,12 +209,253 @@ public class ProfileFragment extends Fragment
                 editLastName.setText(user.getLastName());
                 editEmail.setText(user.getEmail());
                 editPhoneNumber.setText(Long.toString(user.getPhone()));
+
+                currentUserInfo = new User(user.getEmail(), user.getPassword(),
+                        user.getPhone(), user.getFirstName(), user.getLastName(),
+                        user.getLicensePlates());
+            }
+        });
+
+        Log.d(TAG, "Loaded user: " + currentUserInfo.toString());
+    }
+
+    private void setupTextFieldsForEditing() {
+        rootView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                okayToError = true;
+            }
+        });
+
+        firstNameInputLayout = rootView.findViewById(R.id.firstName_InputLayout);
+        firstNameInputLayout.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(okayToError) {
+                    Utils.checkName(firstNameInputLayout, getString(R.string.cannot_be_empty));
+                }
+            }
+        });
+
+        lastNameInputLayout = rootView.findViewById(R.id.lastName_InputLayout);
+        lastNameInputLayout.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(okayToError) {
+                    Utils.checkName(lastNameInputLayout, getString(R.string.cannot_be_empty));
+                }
+            }
+        });
+
+        emailInputLayout = rootView.findViewById(R.id.email_InputLayout);
+        emailInputLayout.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(okayToError) {
+                    Utils.checkEmail(emailInputLayout, getString(R.string.invalid_email));
+                }
+            }
+        });
+
+        phoneInputLayout = rootView.findViewById(R.id.phoneNumber_InputLayout);
+        phoneInputLayout.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(okayToError) {
+                    Utils.checkPhone(phoneInputLayout, getString(R.string.invalid_phone_number));
+                }
+            }
+        });
+
+        passwordInputLayout = rootView.findViewById(R.id.password_InputLayout);
+        passwordInputLayout.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(okayToError) {
+                    Utils.checkPassword(passwordInputLayout, getString(R.string.invalid_password));
+                }
+            }
+        });
+
+        confirmInputLayout = rootView.findViewById(R.id.confirmPassword_InputLayout);
+        confirmInputLayout.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(okayToError) {
+                    Utils.checkConfirm(passwordInputLayout, confirmInputLayout,
+                            getString(R.string.passwords_do_not_match));
+                }
             }
         });
     }
 
     public void updateAccount() {
-        //TODO
+        if(!editFirstName.getText().toString().equals(currentUserInfo.getFirstName())
+                && Utils.checkName(firstNameInputLayout, getString(R.string.cannot_be_empty))) {
+
+            String newFirstName = editFirstName.getText().toString();
+
+            Log.d(TAG, "First name update to " + newFirstName
+                    + " from " + currentUserInfo.getFirstName());
+
+            mainActivityViewModel.updateUserField(currentUserInfo,
+                    FIELD_FIRST_NAME, newFirstName);
+            currentUserInfo.setFirstName(newFirstName);
+        } else {
+            Log.d(TAG, "No change in first name.");
+        }
+
+        if(!editLastName.getText().toString().equals(currentUserInfo.getLastName())
+                && Utils.checkName(lastNameInputLayout, getString(R.string.cannot_be_empty))) {
+
+            String newLastName = editLastName.getText().toString();
+
+            Log.d(TAG, "First name update to " + newLastName
+                    + " from " + currentUserInfo.getLastName());
+
+            mainActivityViewModel.updateUserField(currentUserInfo,
+                    FIELD_LAST_NAME, newLastName);
+            currentUserInfo.setLastName(newLastName);
+        } else {
+            Log.d(TAG, "No change in last name.");
+        }
+
+        //TODO FIX
+        if(!editEmail.getText().toString().equals(currentUserInfo.getEmail())
+                && Utils.checkName(emailInputLayout, getString(R.string.invalid_email))) {
+
+            String newEmail = editEmail.getText().toString().toLowerCase();
+
+            Log.d(TAG, "Email update to " + newEmail
+                    + " from " + currentUserInfo.getEmail());
+
+            mainActivityViewModel.updateUserField(currentUserInfo,
+                    FIELD_EMAIL, newEmail);
+
+            currentUserInfo.setEmail(newEmail);
+
+            currentFirebaseUser.updateEmail(newEmail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    Log.d(TAG, "FirebaseAuth email Changed.");
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.d(TAG, "FirebaseAuth email change error");
+                }
+            });
+
+        } else {
+            Log.d(TAG, "No change in email.");
+        }
+
+        if(!editPhoneNumber.getText().toString().equals(currentUserInfo.getPhone())
+                && Utils.checkPhone(phoneInputLayout, getString(R.string.invalid_phone_number))) {
+
+            String newPhone = editPhoneNumber.getText().toString();
+
+            Log.d(TAG, "Phone update to " + newPhone
+                    + " from " + currentUserInfo.getPhone());
+
+            mainActivityViewModel.updateUserField(currentUserInfo,
+                    FIELD_PHONE, newPhone);
+
+            currentUserInfo.setPhone(Long.valueOf(newPhone));
+
+        } else {
+            Log.d(TAG, "No change in phone number.");
+        }
+
+        if(!editPassword.getText().toString().equals(currentUserInfo.getPassword())
+                && Utils.checkConfirm(passwordInputLayout, confirmInputLayout, getString(R.string.invalid_email))) {
+
+            String newPassword = editPassword.getText().toString();
+
+            Log.d(TAG, "Password update to " + newPassword
+                    + " from " + currentUserInfo.getPassword());
+
+            mainActivityViewModel.updateUserField(currentUserInfo,
+                    FIELD_PASSWORD, newPassword);
+
+            currentUserInfo.setPassword(newPassword);
+
+            currentFirebaseUser.updatePassword(newPassword).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    Log.d(TAG, "FirebaseAuth password Changed.");
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.d(TAG, "FirebaseAuth password change error");
+                }
+            });
+
+        } else {
+            Log.d(TAG, "No change in password.");
+        }
+
     }
 
     public void createAccount() {
